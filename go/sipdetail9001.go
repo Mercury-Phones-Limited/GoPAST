e9102a2ab094b08a2a78956b97eff831e9626723
Elliot Keavney <103308993+Ellwould@users.noreply.github.com>
2023-03-05 12:10:12 +0000

    Update sipdetail9001.go

--- go/sipdetail9001.go
+++ go/sipdetail9001.go
@@ -49,7 +49,6 @@ func main() {
 		}
 
 		fmt.Fprintf(w, startHTML)
-		fmt.Fprintf(w, "<a href=\"https://"+domainName+"/sip-registration\" class=\"buttonsip\">SIP Registration</a> &nbsp &nbsp &nbsp")
 		fmt.Fprintf(w, "<a href=\"https://"+domainName+"/\" class=\"buttonsip\">Main Menu</a> &nbsp &nbsp &nbsp")
                 fmt.Fprintf(w, "<a href=\"https://"+domainName+"/sip-registration\" class=\"buttonsip\">SIP Registration</a> &nbsp &nbsp &nbsp")
                 fmt.Fprintf(w, "<a href=\"https://"+domainName+"/alter-sip\" class=\"buttonsip\">Alter SIP</a> &nbsp &nbsp &nbsp")
