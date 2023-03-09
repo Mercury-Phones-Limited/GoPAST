e73a06fb92911b1de09daf0171a68b6f8fd7f168
Elliot Keavney <103308993+Ellwould@users.noreply.github.com>
2023-03-09 22:05:37 +0000

    Update user_post_9003.go

--- go/user_post_9003.go
+++ go/user_post_9003.go
@@ -41,7 +41,7 @@ func main() {
 	var endHTML string
 	endHTML = cpresource.EndHTML()
 
-	http.HandleFunc("/add-alter-sip", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/user-add-alter-sip", func(w http.ResponseWriter, r *http.Request) {
 
 		fmt.Fprintf(w, startHTML)
 		fmt.Fprintf(w, "<table>")
