2a9faf63402919b6b861e3069b61ee6f0603c8b2
Elliot Keavney <103308993+Ellwould@users.noreply.github.com>
2023-03-16 13:22:39 +0000

    Update user_sip_detail_9001.go

--- go/user_sip_detail_9001.go
+++ go/user_sip_detail_9001.go
@@ -90,10 +90,15 @@ func main() {
 			}
 
 			fmt.Fprintf(w, startHTML)
-			fmt.Fprintf(w, "<a href=\"https://"+domainName+"/\" class=\"sipbutton\">Main Menu</a> &nbsp &nbsp &nbsp")
-			fmt.Fprintf(w, "<a href=\"https://"+domainName+"/user-sip-registration\" class=\"sipbutton\">SIP Registration</a> &nbsp &nbsp &nbsp")
-			fmt.Fprintf(w, "<a href=\"https://"+domainName+"/user-sip-post\" class=\"addalterbutton\">Add / Alter SIP</a> &nbsp &nbsp &nbsp")
+			fmt.Fprintf(w, "&nbsp &nbsp &nbsp")
+			fmt.Fprintf(w, "<a href=\"https://"+domainName+"/\" class=\"sipbutton\">Main Menu</a>")
+			fmt.Fprintf(w, "&nbsp &nbsp &nbsp")
+			fmt.Fprintf(w, "<a href=\"https://"+domainName+"/user-sip-registration\" class=\"sipbutton\">SIP Registration</a>")
+			fmt.Fprintf(w, "&nbsp &nbsp &nbsp")
+			fmt.Fprintf(w, "<a href=\"https://"+domainName+"/user-sip-post\" class=\"addalterbutton\">Add / Alter SIP</a>")
+			fmt.Fprintf(w, "&nbsp &nbsp &nbsp")
 			fmt.Fprintf(w, "<a href=\"https://"+domainName+"/oauth2/sign_out\" class=\"logoutbutton\">Logout</a>")
+			fmt.Fprintf(w, "&nbsp &nbsp &nbsp")
 			fmt.Fprintf(w, "<br>")
 			fmt.Fprintf(w, "<br>")
 			fmt.Fprintf(w, "<br>")
